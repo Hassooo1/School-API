d4c9fbd83154f93ca716fad5154723befa02357f
Hasnain Khan <hasnainkhan9446@gmail.com>
2023-02-01 23:14:16 +0500

    Added a urls to urls.py

--- academic/urls.py
+++ academic/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import StudentList, StudentDetail, TeacherList, TeacherDetail, ClassList, ClassDetail
+from .views import StudentList, StudentDetail, TeacherList, TeacherDetail, ClassList, ClassDetail, AssignmentList, AssignmenDetail
 
 urlpatterns = [
     path('students/', StudentList.as_view(), name='student-list'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('teachers/<int:pk>/', TeacherDetail.as_view(), name='teacher-details'),
     path('classes/', ClassList.as_view(), name='class-list'),
     path('classes/<int:pk>/', ClassDetail.as_view(), name='class-details'),
+    path('assignments/', AssignmentList.as_view(), name='assignment-list'),
+    path('assignments/<int:pk>/', AssignmenDetail.as_view(), name='assignment-details'),
 ]
