241418e03ac2c234a66ae747203e29931480ec4d
Hasnain Khan <hasnainkhan9446@gmail.com>
2023-02-01 21:23:44 +0500

    Added a TeacherList and TeacherDetail classes in views.py

--- academic/views.py
+++ academic/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Student
-from .serializers import StudentSerializer
+from .models import Student, Teacher
+from .serializers import StudentSerializer, TeacherSerializer
 
 class StudentList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
@@ -9,3 +9,11 @@ class StudentList(generics.ListCreateAPIView):
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+class TeacherList(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
