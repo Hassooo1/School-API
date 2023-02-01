94fd89f707694eaa7aa9e1f39e6c09d08dce7d47
Hasnain Khan <hasnainkhan9446@gmail.com>
2023-02-01 21:26:39 +0500

    Added a TeacherList and TeacherDetail in urls.py

--- academic/urls.py
+++ academic/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import StudentList, StudentDetail
+from .views import StudentList, StudentDetail, TeacherList, TeacherDetail
 
 urlpatterns = [
     path('students/', StudentList.as_view(), name='student-list'),
-    path('students/<int:pk>/', StudentDetail.as_view(), name='student-details')
+    path('students/<int:pk>/', StudentDetail.as_view(), name='student-details'),
+    path('teachers/', TeacherList.as_view(), name='teacher-list'),
+    path('teachers/<int:pk>/', TeacherDetail.as_view(), name='teacher-details'),
 
 ]
