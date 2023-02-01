8504e242ee90e3dcc396d9110dba111668ab1b9d
Hasnain Khan <hasnainkhan9446@gmail.com>
2023-02-01 21:15:53 +0500

    Added a Teacher class in models.py

--- academic/models.py
+++ academic/models.py
@@ -12,3 +12,15 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    gender = models.CharField(max_length=10)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
