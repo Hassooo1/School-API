4278b72c5180635104c38ac6e7925f5217f47f69
Hasnain Khan <hasnainkhan9446@gmail.com>
2023-02-01 22:35:43 +0500

    makingmigration and migrate

--- academic/models.py
+++ academic/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from . models import Class
-
-# Create your models here.
-
+#
+#
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.PositiveSmallIntegerField()
@@ -22,7 +20,7 @@ class Teacher(models.Model):
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField()
-    class_taught = models.ManyToManyField(Class, related_name='teachers')
+    class_taught = models.ManyToManyField('Class', related_name='teachers')
 
     def __str__(self):
         return self.name
