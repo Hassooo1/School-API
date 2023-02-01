19fc374809ae01102a0ab6e73b51953c201dc07f
Hasnain Khan <hasnainkhan9446@gmail.com>
2023-02-01 18:37:24 +0500

    first we make a modle Student, then we make and run migrations, then we make a serializer class called StudentSerializer, then we make a StudentList class in views and make a url and also add this url in student_api.urls

--- academic/models.py
+++ academic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    gender = models.CharField(max_length=10)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
